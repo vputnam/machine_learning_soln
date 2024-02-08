b442714a7e7a360718fd556b57370a9fcfc44192
Vanessa Putnam <vputnam99@gmail.com>
2024-02-07 20:52:54 -0800

    inital dotnet api code

---
+++ function_app.py
@@ -0,0 +1,28 @@
+import azure.functions as func
+import logging
+import numpy as np
+import pickle
+import json
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="Predict")
+def Predict(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
